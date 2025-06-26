6804831da27e98262efff29ffa7061d8c65d469a
JaskiratSudan <jaskiratsinghsudan@gmail.com>
2025-06-26 14:47:31 -0400

    new structure

--- YOLO2/yolo_to_kotlin.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load trained YOLO model
-# model = YOLO("multiclass_det_03-26-25.pt")
-# model = YOLO("content/YOLOv11_det_640/yolov11_det_640/weights/best.pt")
-model = YOLO("Gtapenail2/weights/best.pt")
-
-model.export(format="tflite")
